30525b6e5eb43249d80ff8334e0807706562550e
Raphaël Gaziano <r.gaziano@gmail.com>
2012-12-24 19:48:09 +0100

    quick tools refacto

--- tools/updateGui.py
+++ tools/updateGui.py
@@ -12,9 +12,13 @@ import os
 
 SOURCE_PATH = os.path.join('lightapp', 'ui', 'QDesigner')
 DEST_PATH   = os.path.join('lightapp', 'ui')
+PYUIC_PATH  = os.path.join('tools', 'pyuic.py')
 
 for f in os.listdir(SOURCE_PATH):
-    cmd = 'python tools/pyuic.py ' + os.path.join(SOURCE_PATH, f) + \
-        ' -o ' + os.path.join(DEST_PATH, f.replace('.ui', '.py'))
+    cmd = 'python %s %s -o %s' % (
+                    PYUIC_PATH,
+                    os.path.join(SOURCE_PATH, f),
+                    os.path.join(DEST_PATH, f.replace('.ui', '.py'))
+    )
     print(cmd)
     os.system(cmd)
