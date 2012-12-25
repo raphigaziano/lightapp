906e675dec1ceba2c3877362e6ab40866350be18
Raphaël Gaziano <r.gaziano@gmail.com>
2012-12-25 17:22:19 +0100

    refacto

--- main.py
+++ main.py
@@ -15,33 +15,38 @@ class LightApp(QtGui.QMainWindow, MainWindow.Ui_MainWindow):
         super(LightApp, self).__init__(parent)
         self.setupUi(self)
         
-        self.load_docs()
-        
-        self.connect_events()
-        
-    def load_docs(self):
-        '''
-        '''
-        self.items = []
-        for s in show.load_shows(config.MASTER_SAVE_PATH):
-            self.items.append(s)
-            i = len(self.items) -1
-            self.table_shows.insertRow(i)
-            
-            newitem = customwidgets.ShowTableItem(s)
-            self.table_shows.setItem(i, 0, newitem)
-            
-            btn_infos = QtGui.QPushButton(self.table_shows)
-            btn_infos.setText('Infos')
-            btn_edit  = QtGui.QPushButton(self.table_shows)
-            btn_edit.setText('Edit')
-            
-            self.table_shows.setCellWidget(i, 1, btn_infos)
-            self.table_shows.setCellWidget(i, 2, btn_edit)
- 
         self.table_shows.setColumnWidth(0, 260)
         self.table_shows.setColumnWidth(1, 50)
         self.table_shows.setColumnWidth(2, 50)
+        
+        self.load_shows()
+        
+        self.connect_events()
+        
+    def add_show(self, show):
+        '''Add a single show to the list and table'''
+        self.shows.append(show)
+        i = len(self.shows) -1
+        self.table_shows.insertRow(i)
+        
+        newitem = customwidgets.ShowTableItem(show)
+        self.table_shows.setItem(i, 0, newitem)
+        
+        btn_infos = QtGui.QPushButton(self.table_shows)
+        btn_infos.setText('Infos')
+        btn_edit  = QtGui.QPushButton(self.table_shows)
+        btn_edit.setText('Edit')
+        
+        self.table_shows.setCellWidget(i, 1, btn_infos)
+        self.table_shows.setCellWidget(i, 2, btn_edit)
+
+        
+    def load_shows(self):
+        '''
+        '''
+        self.shows = []
+        for s in show.load_shows(config.MASTER_SAVE_PATH):
+            self.add_show(s)
  
     def connect_events(self): pass
  
